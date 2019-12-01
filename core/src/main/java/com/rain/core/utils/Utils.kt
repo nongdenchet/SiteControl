49796026da8af29b0bc2e85c4e848684070b6225
Quan Vu <nongdenchet8435848@gmail.com>
2019-12-01 17:42:55 +0800

    (CONFIG) Update app icon

--- core/src/main/java/com/rain/core/utils/Utils.kt
+++ core/src/main/java/com/rain/core/utils/Utils.kt
@@ -6,31 +6,14 @@ import android.net.Uri
 import android.os.Build
 import android.provider.Settings
 import android.provider.Settings.ACTION_MANAGE_OVERLAY_PERMISSION
-import android.util.DisplayMetrics
 import android.view.View
 import android.view.WindowManager
 import android.widget.EditText
-import android.widget.ImageView
-import androidx.annotation.DrawableRes
-import com.bumptech.glide.Glide
 import com.jakewharton.rxbinding2.view.RxView
 import com.jakewharton.rxbinding2.widget.RxTextView
-import io.reactivex.Flowable
 import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.functions.BiFunction
-import io.reactivex.functions.Function
-import org.reactivestreams.Publisher
-import java.text.NumberFormat
-import java.util.Locale
 import java.util.concurrent.TimeUnit
-
-fun setText(editText: EditText, value: String) {
-    if (editText.text.toString() != value) {
-        editText.setText(value)
-        editText.setSelection(value.length)
-    }
-}
 
 fun hasOverlayPermission(context: Context): Boolean {
     return Build.VERSION.SDK_INT < Build.VERSION_CODES.M || Settings.canDrawOverlays(context.applicationContext)
@@ -47,7 +30,11 @@ fun toOverlayPermission(context: Context) {
     if (!hasOverlayPermission(context)) {
         val intent = Intent()
         intent.action = ACTION_MANAGE_OVERLAY_PERMISSION
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS)
+        intent.addFlags(
+            Intent.FLAG_ACTIVITY_NEW_TASK
+                    or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                    or Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS
+        )
         intent.data = Uri.parse("package:${context.packageName}")
         context.startActivity(intent)
     }
@@ -55,37 +42,12 @@ fun toOverlayPermission(context: Context) {
 
 fun toAccessibilityPermission(context: Context) {
     val intent = Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS)
-    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS)
+    intent.addFlags(
+        Intent.FLAG_ACTIVITY_NEW_TASK
+                or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                or Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS
+    )
     context.startActivity(intent)
-}
-
-fun exponentialBackoff(
-    times: Int,
-    delay: Long
-): Function<in Flowable<Throwable>, out Publisher<*>> {
-    return Function { error ->
-        error.zipWith(Flowable.range(1, times), BiFunction<Throwable, Int, Int> { _, i -> i })
-            .flatMap { Flowable.timer(delay * it, TimeUnit.SECONDS) }
-    }
-}
-
-fun setMoney(editText: EditText, value: String) {
-    if (editText.text.toString() != value) {
-        try {
-            val numberFormatter = NumberFormat.getNumberInstance()
-            val number = numberFormatter.parse(value)
-            val result = numberFormatter.format(number)
-            setText(editText, result)
-        } catch (e: Exception) {
-            setText(editText, "")
-        }
-    }
-}
-
-fun loadIcon(imageView: ImageView, @DrawableRes icon: Int) {
-    Glide.with(imageView.context)
-        .load(icon)
-        .into(imageView)
 }
 
 fun getOverlayType(): Int {
@@ -93,12 +55,6 @@ fun getOverlayType(): Int {
         return WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY
     }
     return WindowManager.LayoutParams.TYPE_PHONE
-}
-
-fun getScreenSize(windowManager: WindowManager): DisplayMetrics {
-    val displayMetrics = DisplayMetrics()
-    windowManager.defaultDisplay.getMetrics(displayMetrics)
-    return displayMetrics
 }
 
 fun getStreamText(editText: EditText): Observable<String> {
@@ -109,14 +65,6 @@ fun getStreamText(editText: EditText): Observable<String> {
         .map { it.text() }
         .map { it.toString() }
         .subscribeOn(AndroidSchedulers.mainThread())
-}
-
-fun getLocale(context: Context): Locale {
-    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-        context.resources.configuration.locales[0]
-    } else {
-        context.resources.configuration.locale
-    }
 }
 
 fun getClicks(view: View): Observable<Any> {
--- onboarding/src/main/java/com/rain/onboarding/ui/OnboardingPresenter.kt
+++ onboarding/src/main/java/com/rain/onboarding/ui/OnboardingPresenter.kt
@@ -4,13 +4,13 @@ import com.rain.onboarding.R
 import com.rain.onboarding.permission.PermissionAction
 import com.rain.onboarding.permission.PermissionManager
 
-class OnboardingPresenter(private val permissionManager: PermissionManager,
-                          private val onboardingView: OnboardingView) {
-
+class OnboardingPresenter(
+    private val permissionManager: PermissionManager,
+    private val onboardingView: OnboardingView
+) {
     fun checkPermission() {
         val permissions = permissionManager.checkPermissions()
-                .map { toViewModel(it) }
-
+            .map { toViewModel(it) }
         if (permissions.isEmpty()) {
             onboardingView.close()
         } else {
@@ -21,14 +21,14 @@ class OnboardingPresenter(private val permissionManager: PermissionManager,
     private fun toViewModel(action: PermissionAction): OnboardingViewModel {
         return when (action) {
             is PermissionAction.Accessibility -> OnboardingViewModel(
-                    R.drawable.ic_accessibility,
-                    R.string.accessibility_description,
-                    action
+                R.drawable.ic_accessibility,
+                R.string.accessibility_description,
+                action
             )
             is PermissionAction.Overlay -> OnboardingViewModel(
-                    R.drawable.ic_overlay,
-                    R.string.overlay_description,
-                    action
+                R.drawable.ic_overlay,
+                R.string.overlay_description,
+                action
             )
         }
     }
